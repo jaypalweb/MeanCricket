ced44ed618f78442496f2e782e7c54b9667441df
Jay Pal <contactjaypal@gmail.com>
2020-06-05 22:30:54 +0530

    home and teams page

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { TeamsComponent } from './teams/teams.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    TeamsComponent
   ],
   imports: [
     BrowserModule,
